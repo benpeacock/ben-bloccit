297dc54ea541d88d01c1fdf0205ef415b0ad5eaf
Ben <ben9910@gmail.com>
2014-02-11 02:56:52 +0000

    Implementing Facebook Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :avatar, :provider, :uid
@@ -10,7 +11,26 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   before_create :set_member
+  has_many :votes, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
+
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      pass = Devise.friendly_token[0,20]
+      user = User.new(name: auth.extra.raw_info.name,
+                         provider: auth.provider,
+                         uid: auth.uid,
+                         email: auth.info.email,
+                         password: pass,
+                         password_confirmation: pass
+                        )
+      user.skip_confirmation!
+      user.save
+    end
+    user
+  end
+
 
 ROLES = %w[member moderator admin]
   def role?(base_role)
