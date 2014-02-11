7c5938e796bb3832ea6f70d9fc9c6689c9bb8509
Ben <ben9910@gmail.com>
2014-02-11 03:44:28 +0000

    Adding links to voting feature

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Bloccit::Application.routes.draw do
 	resources :topics do
 		resources :posts do
 			resources :comments, only: [:create]
+			match '/up-vote', to: 'votes#up_vote', as: :up_vote
+      match '/down-vote', to: 'votes#down_vote', as: :down_vote
 		end
 	end
 
