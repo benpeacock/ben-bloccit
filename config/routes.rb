fa3ba5acc900f5851c19b3e3e68b64397dab7c8c
Ben <ben9910@gmail.com>
2014-02-14 03:29:10 +0000

    Implementing public profiles, but having routing errors

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Bloccit::Application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
 
+  resources :users, only: [:show] # create a route for users#show
 	resources :topics do
 		resources :posts do
 			resources :comments, only: [:create]
-			match '/up-vote', to: 'votes#up_vote', as: :up_vote
-      match '/down-vote', to: 'votes#down_vote', as: :down_vote
-      resources :favorites, only: [:create, :destroy]
+				match '/up-vote', to: 'votes#up_vote', as: :up_vote
+      	match '/down-vote', to: 'votes#down_vote', as: :down_vote
+      	resources :favorites, only: [:create, :destroy]
 		end
 	end
 
