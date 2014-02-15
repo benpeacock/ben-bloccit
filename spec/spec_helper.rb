8582873dac8d0a89a3750efbb213b4b0bdc5595d
Ben <ben9910@gmail.com>
2014-02-15 21:56:30 +0000

    Adding user test, but getting error message

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -33,6 +33,6 @@ RSpec.configure do |config|
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
-  #     --seed 1234
+  # --seed 1234
   config.order = "random"
 end
